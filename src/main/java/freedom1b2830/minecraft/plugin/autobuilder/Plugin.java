78b59210c62b0b1c0823d4af314468961f25550c
freedom1b2830 <freedom1b2830@gmail.com>
2022-08-01 20:09:14 +0000

    config file

--- src/main/java/freedom1b2830/minecraft/plugin/autobuilder/Plugin.java
+++ src/main/java/freedom1b2830/minecraft/plugin/autobuilder/Plugin.java
@@ -1,8 +1,18 @@
 package freedom1b2830.minecraft.plugin.autobuilder;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import freedom1b2830.minecraft.plugin.autobuilder.config.BuilderConfig;
 import freedom1b2830.minecraft.plugin.autobuilder.config.BuilderEntity;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class Plugin extends JavaPlugin {
@@ -46,6 +56,51 @@ public class Plugin extends JavaPlugin {
     public void onEnable() {
         if (!getInstance().started) {
             getInstance().executor.start();
+        }
+        try {
+            readConfig();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static final File pluginsDir = getPluginsDir();
+    public static final File builderDir = getBuilderDir();
+
+    private static File getBuilderDir() {
+        return new File(getPluginsDir(), getInstance().getName());
+    }
+
+    private static File getPluginsDir() {
+        try {
+            return findByFileName(new File("").toPath(), "plugins").get(0).toFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static List<Path> findByFileName(Path path, String fileName)
+            throws IOException {
+        List<Path> result;
+        try (Stream<Path> pathStream = Files.find(path, Integer.MAX_VALUE, (p, basicFileAttributes) -> p.getFileName().toString().equalsIgnoreCase(fileName))) {
+            result = pathStream.collect(Collectors.toList());
+        }
+        return result;
+    }
+
+    public static final File configFile = new File(builderDir, "builder.config.yaml");
+
+    public static final ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+
+    private void readConfig() throws IOException {
+        if (!builderDir.exists()) {
+            Files.createDirectories(builderDir.toPath());
+        }
+        if (configFile.exists()) {
+            config = mapper.readValue(configFile, BuilderConfig.class);
+        } else {
+            mapper.writeValue(configFile, config);
+            getInstance().getLogger().info(String.format("edit builder config file: %s , and reload ", configFile));
         }
     }
 }
