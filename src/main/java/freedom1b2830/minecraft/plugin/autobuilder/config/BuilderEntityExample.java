faaecae55792ce343d426e5013c6c7a0b8a66f5c
freedom1b2830 <freedom1b2830@gmail.com>
2022-08-01 23:02:47 +0000

    fix

--- src/main/java/freedom1b2830/minecraft/plugin/autobuilder/config/BuilderEntityExample.java
+++ src/main/java/freedom1b2830/minecraft/plugin/autobuilder/config/BuilderEntityExample.java
@@ -13,7 +13,7 @@ public class BuilderEntityExample extends BuilderEntity {
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
-        exeScript = new File(new File(Plugin.pluginsDir, "freedom1b2830-plugin-autobuilder"), "update.sh");
+        exeScript = new File(new File(Plugin.pluginsDir, "freedom1b2830-plugin-autobuilder-SOURCES"), "update.sh");
         File exeScriptTmp = new File(exeScript.getAbsolutePath());//FIX NPE for "exeScript.getAbsolutePath()"
         exeDir = new File(exeScriptTmp.getParentFile().getAbsolutePath());
         repoDir = exeDir;
